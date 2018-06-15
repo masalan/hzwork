a01f8ea9cec1e4cc61879bb975c012f4b815911f
Alain serge <mussiruserge@yahoo.fr>
2018-06-15 17:31:46 +0800

    Alan

---
+++ UniversalApp/Modules/Mine/View/mineTableDataView.h
@@ -0,0 +1,13 @@
+//
+//  mineTableDataView.h
+//  UniversalApp
+//
+//  Created by 诺虹科技 on 15/06/2018.
+//  Copyright © 2018 Alain Serge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mineTableDataView : UITableView
+
+@end
